c5e432c221c4aa921ce6feb0f2329fae48248411
Juliano Galon <37384932+julianogalon@users.noreply.github.com>
2025-05-27 07:31:35 -0300

    Update main.py
    correções na chamada das dependências/classes

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
 from data.csv_importador import CSVImportador
-from analysis.monetizacao import ModeloMonetizacaoEstatistica
+from analysis.monetizacao import ModeloMonetizaçãoEstatistica
 from analysis.ano_lancamento import AnoLancamentoEstatistica
-from analysis.plataforma_os import EstatisticaPlataformaOS
-from analysis.estatistica_linhas import EstatisticaLinhasCSV
+from analysis.plataforma_os import PlataformaOSEstatistica
 
 # Execução do Programa Principal para análise estatística:
-importador = CSVImportador('../steam_games.csv')
+importador = CSVImportador('/content/steam_games.csv')
+#importador = CSVImportador('/content/steam_games_test.csv')
 dados = importador.importar()
 
-analiseMonetizacao = ModeloMonetizacaoEstatistica(dados)
+# Pergunta 1
+analiseMonetizacao = ModeloMonetizaçãoEstatistica(dados)
 analiseMonetizacao.resumo()
 
+# Pergunta 2
 analiseLancamentoAno = AnoLancamentoEstatistica(dados)
 analiseLancamentoAno.mostrar_anos_mais_lancamentos()
 
-analisePlataformaOS = EstatisticaPlataformaOS(dados)
+# Pergunta 3
+analisePlataformaOS = PlataformaOSEstatistica(dados)
 analisePlataformaOS.resumo()
