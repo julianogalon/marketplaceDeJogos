1af0402cbd40eaa372dbad3030fcd657558012b0
Juliano Galon <37384932+julianogalon@users.noreply.github.com>
2025-05-28 05:33:27 -0300

    Update main.py
    adaptando para rodar no modo teste ou no modo real

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
+import sys
 from data.csv_importador import CSVImportador
 from analysis.monetizacao import ModeloMonetizaçãoEstatistica
 from analysis.ano_lancamento import AnoLancamentoEstatistica
 from analysis.plataforma_os import PlataformaOSEstatistica
 
 # Execução do Programa Principal para análise estatística:
-importador = CSVImportador('/content/steam_games.csv')
-#importador = CSVImportador('/content/steam_games_test.csv')
-dados = importador.importar()
+def main():
+    # Verifica se foi passado '-teste' como argumento
+    if '-teste' in sys.argv:
+        arquivo_csv = '/content/steam_games_test.csv'
+        print("Executando em modo TESTE")
+    else:
+        arquivo_csv = '/content/steam_games.csv'
+        print("Executando em modo PRODUÇÃO")
+
+    importador = CSVImportador(arquivo_csv)
+    dados = importador.importar()
 
 # Pergunta 1
 analiseMonetizacao = ModeloMonetizaçãoEstatistica(dados)
@@ -19,3 +28,6 @@ analiseLancamentoAno.mostrar_anos_mais_lancamentos()
 # Pergunta 3
 analisePlataformaOS = PlataformaOSEstatistica(dados)
 analisePlataformaOS.resumo()
+
+if __name__ == "__main__":
+    main()
